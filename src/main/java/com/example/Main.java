f59ee0ea0a977bdd599f7c4f6316c746a2043b11
JosuelSantosDEV <josuelsantosdev@gmail.com>
2025-06-12 14:37:19 -0300

    Primeiro commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello world!");
+        
+    }
+
+    public class Teste {
+        private final  String nome = "teste";
+
+        public String getNome() {
+            return nome;
+        }
+
+    }
+}
---
+++ src/main/java/com/example/atividade_01/atv_01.java
@@ -0,0 +1,18 @@
+package com.example.atividade_01;
+
+import java.util.Scanner;
+
+public class atv_01 {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+    public int ler(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
